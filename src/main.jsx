f12c7b7947f539f2ece096c787be1629e8026f28
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-11-27 12:47:48 +0600

    add authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Toaster } from 'react-hot-toast';
 
 import {
   createBrowserRouter,
@@ -8,6 +9,7 @@ import {
 } from "react-router-dom";
 import Roots from './Roots/Roots';
 import Home from './Components/Pages/Home/Home';
+import AuthProvider from './Components/Auth/AuthProvider';
 
 
 const router = createBrowserRouter([
@@ -27,7 +29,10 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='w-full mx-auto'>
-      <RouterProvider router={router}></RouterProvider>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
+      <Toaster />
     </div>
   </React.StrictMode>,
 )
